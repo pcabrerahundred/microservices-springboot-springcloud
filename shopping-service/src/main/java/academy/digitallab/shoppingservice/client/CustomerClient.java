47c085af71d7c09f1f95febe1d7ec0d3327d0642
pcabrerahundred <paul.cabrera@inetum.world>
2021-11-18 18:21:01 -0500

    fix shopping-service config

--- shopping-service/src/main/java/academy/digitallab/shoppingservice/client/CustomerClient.java
+++ shopping-service/src/main/java/academy/digitallab/shoppingservice/client/CustomerClient.java
@@ -7,10 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "customer-service", fallback = CustomerHystrixFallbackFactory.class)
-
 public interface CustomerClient {
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "/customers/{id}")
     public ResponseEntity<Customer> getCustomer(@PathVariable("id") long id);
 
 }
